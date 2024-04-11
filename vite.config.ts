1390606e501546cfa9fd59c290fb9db1544373a8
momo <momo>
2024-04-11 04:36:16 +0000

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { UserConfig, defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/zoom-clone/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  build: {
+    outDir: 'docs'
+  }
+})
